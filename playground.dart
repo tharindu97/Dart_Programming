8a7c54457c7cbf1a15d7716d9b84cc6b072b8abd
Tharindu97 <tharindukavishna@gmail.com>
2020-09-09 18:57:44 +0530

    41

---
+++ playground.dart
@@ -0,0 +1,36 @@
+
+
+// // Data Types
+// main(){
+
+//   int number = 45;
+//   var number1 = 80;
+//   print('Amount : $number | Amount : $number1 \n');
+
+//   double dnumber = 100.11;
+//   var dnumber1 = 200.22;
+//   print('Amount : $dnumber | Amount : $dnumber1 \n');
+
+//   String snumber = 'panama kollo';
+//   var snumber1 = 'panama boys';
+//   print('Amount : $snumber | Amount : $snumber1 \n');
+
+//   bool bnumber = true;
+//   var bnumber1 = false;
+//   print('Amount : $bnumber | Amount : $bnumber1 \n');
+
+//   dynamic weakVariable = 100;
+//   print('Amount : $weakVariable \n');
+
+//   weakVariable = 'Dart Programming';
+//   print('Amount : $weakVariable'); 
+
+// }
+
+
+// import 'dart:io';
+// main(){
+//   stdout.writeln('What is your name');
+//   String name = stdin.readLineSync();
+//   print('my name is $name');
+// }
